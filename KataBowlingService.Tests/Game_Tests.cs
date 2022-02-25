bd5ca3255b05b65102589ad911b4fbceec050a49
SylvainJ66 <100413870+SylvainJ66@users.noreply.github.com>
2022-02-25 11:45:53 +0100

    Add files via upload

---
+++ KataBowlingService.Tests/Game_Tests.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace KataBowlingService.Tests
+{
+    public class Game_Tests
+    {
+        private Game _game;
+
+        public Game_Tests()
+        {
+            _game = new Game();
+        }
+
+        [Fact]
+        public void Roll_AndKnockDown0Pins_ShouldReturn0()
+        {
+            Assert.Equal(0, _game.Roll(0));
+        }
+
+        [Fact]
+        public void Roll_AndKnockDown1Pins_ShouldReturn1()
+        {
+            Assert.Equal(1, _game.Roll(1));
+        }
+
+        [Fact]
+        public void Roll_AndKnockDown11Pins_ShouldReturnException()
+        {
+            Assert.Throws<Exception>(() => _game.Roll(11));
+        }
+
+        [Fact]
+        public void Roll_AndKnockDown12Pins_ShouldReturnException()
+        {
+            Assert.Throws<Exception>(() => _game.Roll(12));
+        }
+
+        [Fact]
+        public void Roll_20Times0ShouldReturn0()
+        {
+            int score = 0;
+            Enumerable.Range(0, 20).ToList().ForEach(_ => score += _game.Roll(0));
+
+            Assert.Equal(0, score);
+
+        }
+
+        [Fact]
+        public void Roll_20Times1ShouldReturn20()
+        {
+            int score = 0;
+            Enumerable.Range(0, 20).ToList().ForEach(_ => score += _game.Roll(1));
+
+            Assert.Equal(20, score);
+
+        }
+
+        [Fact]
+        public void Roll_ASpareOnTheFirstFrame()
+        {
+            _game.Roll(5);
+            _game.Roll(5);
+            _game.Roll(3);
+            _game.Roll(0);
+            int finalScore = this.FinishTheGame(16, 0);
+            Assert.Equal(16, finalScore);
+        }
+
+        [Fact]
+        public void Roll_ASpareOnTheSecondFrame()
+        {
+            _game.Roll(1);
+            _game.Roll(1);
+            _game.Roll(5);
+            _game.Roll(5);
+            _game.Roll(3);
+            int finalScore = this.FinishTheGame(15, 0);
+            Assert.Equal(18, finalScore);
+        }
+
+        [Fact]
+        public void Roll_AStikeOnTheFirstFrame()
+        {
+            _game.Roll(10);
+            _game.Roll(5);
+            _game.Roll(5);
+            int finalScore = this.FinishTheGame(17, 0);
+            Assert.Equal(30, finalScore);
+        }
+
+        [Fact]
+        public void Roll_SpareGiveAnExtraBallOnTheThenthFrame()
+        {
+            _game.Roll(5);
+            _game.Roll(5);
+            _game.Roll(3);
+            Enumerable.Range(0, 17).ToList().ForEach(_ => _game.Roll(0));
+            _game.Roll(3);
+            int finalScore = _game.Score();
+            Assert.Equal(19, finalScore);
+        }
+
+        [Fact]
+        public void Roll_StrikeGiveAnExtraBallOnTheThenthFrame()
+        {
+            _game.Roll(10);
+            _game.Roll(5);
+            _game.Roll(3);
+            Enumerable.Range(0, 17).ToList().ForEach(_ => _game.Roll(0));
+            _game.Roll(2);
+            int finalScore = _game.Score();
+            Assert.Equal(30, finalScore);
+        }
+
+
+        private int FinishTheGame(int rollsNumber, int pinsKnockedDown)
+        {
+            Enumerable.Range(0, rollsNumber).ToList().ForEach(_ => _game.Roll(pinsKnockedDown));
+            return _game.Score();
+        }
+    }
+}
---
+++ KataBowlingService/Game.cs
@@ -0,0 +1,74 @@
+﻿namespace KataBowlingService;
+public class Game
+{
+    private List<int> _Rolls = new List<int>();
+
+    /// <summary>
+    /// called each time the player rolls a ball.  
+    /// The argument is the number of pins knocked down.
+    /// </summary>
+    /// <returns>
+    /// 
+    /// </returns>
+    public int Roll(int pinsKnockedDown)
+    {
+        if(pinsKnockedDown > 10)
+        {
+            throw new Exception();
+        }
+
+        this._Rolls.Add(pinsKnockedDown);    
+
+        return pinsKnockedDown;
+    }
+
+    /// <summary>
+    /// called only at the very end of the game.  
+    /// It returns the total score for that game
+    /// </summary>
+    /// <returns></returns>
+    public int Score()
+    {
+        int score = 0;
+        int rolls = 0;
+        bool anySpareOrStrike = false;
+
+        for(int frame = 0; frame < 10; frame++)
+        {
+            if(this.IsSpare(rolls))
+            {
+                score += 10 + this._Rolls[rolls + 2];
+                rolls += 2;
+                anySpareOrStrike = true;
+            }
+            else if(this.IsStrike(rolls))
+            {
+                score += 10 + this._Rolls[rolls + 1] + this._Rolls[rolls + 1];
+                rolls++;
+                anySpareOrStrike = true;
+            }
+            else
+            {
+                score += this._Rolls[rolls] + this._Rolls[rolls + 1];
+                rolls += 2;
+            }
+        }
+
+        if (anySpareOrStrike)
+        {
+            score += this._Rolls.LastOrDefault();
+        }
+
+        return score;
+    }
+
+    private bool IsSpare(int roll)
+    {
+        return this._Rolls[roll] + this._Rolls[roll + 1] == 10;
+    }
+
+    private bool IsStrike(int roll)
+    {
+        return this._Rolls[roll] == 10;
+    }
+}
